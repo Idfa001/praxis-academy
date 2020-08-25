dda5a21e5e13ac92729090476b30a1edfa17165e
Idfa001 <idfa001@gmail.com>
2020-08-25 16:10:54 +0700

    Hari 2

---
+++ novice/m2/h2/todolist/task/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from . import models
+
+def index (req):
+    if req.POST:
+        models.Task.objects.create(name=req.POST['name'])
+
+    task = models.Task.objects.all()
+    return render(req, 'task/index.html', {
+        'data': task,
+    })
+
+def detail(req, id):
+    task = models.Task.objects.filter(pk=id).first()
+    return render(req, 'task/detail.html', {
+        'data': task,
+    })
+
