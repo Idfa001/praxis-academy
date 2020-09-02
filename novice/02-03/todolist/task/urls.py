803d061c648addd79b720f13015655189014b8b0
Idfa001 <idfa001@gmail.com>
2020-09-02 09:45:56 +0700

    praxis-academy

---
+++ novice/02-03/todolist/task/urls.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('<id>/', views.detail),
+    path('<id>/delete/', views.delete),
+]
