0708913dea5af183e4adcf0cfa027685d07d541e
Idfa001 <idfa001@gmail.com>
2020-09-07 11:02:02 +0700

    praxis

---
+++ novice/04-01/todolist/task/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    artis = models.CharField(max_length=255)
+    th = models.TextField(default='')
+    lirik = models.CharField(max_length=255)
+    link  = models.CharField(max_length=255)
+
+class Movie(models.Model):
+    title = models.TextField(default='')
+    gen   = models.TextField(default='')
+    rate  = models.TextField(default='')
+    years = models.TextField(default='')
+    des   = models.TextField(default='')
---
+++ novice/04-01/todolist/task/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from . import models, forms
+
+def index (req):
+
+    task = models.Task.objects.all()
+    return render(req, 'task/index.html', {
+        'data': task,
+    })
+
+def input (req):
+    form_input = forms.TaskForm()
+
+    if req.POST:
+        form_input = forms.TaskForm(req.POST)
+
+        if form_input.is_valid():
+           form_input.save()
+
+    task = models.Task.objects.all()
+    return render(req, 'task/input.html', {
+        'data': task,
+        'form': form_input,
+    })
+
+def inputmovie (req):
+    if req.POST:
+        models.Movie.objects.create(title=req.POST['title'], gen=req.POST['gen'], rate=req.POST['rate'], years=req.POST['years'], des=req.POST['des'])
+        return redirect('/')
+
+    task = models.Movie.objects.all()
+    return render(req, 'task/input_mov.html', {
+        'data': task,
+    })
+
+def detail(req, id):
+    task = models.Task.objects.filter(pk=id).first()
+    return render(req, 'task/detail.html', {
+        'data': task,
+    })
+
+def detailmovie(req, id):
+    task = models.Movie.objects.filter(pk=id).first()
+    return render(req, 'task/detail_mov.html', {
+        'data': task,
+    })
+
+def delete(req, id):
+    models.Task.objects.filter(pk=id).delete()
+    return redirect('/')
+
+def deletemovie(req, id):
+    models.Movie.objects.filter(pk=id).delete()
+    return redirect('/')
+
+def update (req, id):
+    if req.POST:
+        task = models.Task.objects.filter(pk=id).update(name=req.POST['name'], genre=req.POST['genre'], artis=req.POST['artis'], th=req.POST['th'], lirik=req.POST['lirik'], link=req.POST['link'])
+        return redirect('/')
+
+    task = models.Task.objects.filter(pk=id).first()
+    return render(req, 'task/update.html', {
+        'data': task,
+    })
+
+def updatemovie (req, id):
+    if req.POST:
+        task = models.Movie.objects.filter(pk=id).update(title=req.POST['title'], gen=req.POST['gen'], rate=req.POST['rate'], years=req.POST['years'], des=req.POST['des'])
+        return redirect('/')
+
+    task = models.Movie.objects.filter(pk=id).first()
+    return render(req, 'task/update_mov.html', {
+        'data': task,
+    })
+
+
