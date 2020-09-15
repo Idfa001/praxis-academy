847df7026671b6534e17d615892b1f7c416f231a
Idfa001 <idfa001@gmail.com>
2020-09-15 17:13:22 +0700

    Projek

--- Projek/keuangan/penjualan/models.py
+++ Projek/keuangan/penjualan/models.py
@@ -2,12 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Penjualan(models.Model):
-	tanggal = models.DateField(auto_now_add=True)
+	tanggal = models.DateField()
 	keterangan = models.CharField(default='Keterangan', max_length= 255)
 	jumlah = models.IntegerField()
 	catatan = models.CharField(default='Catatan', max_length= 255)
-	
-	def tanggal_f(self):
-		return self.tanggal.strftime("%d %b %Y ")
+
 
 
