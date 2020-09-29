e5d03bfe407606cbe17482b831afc1e7fd2615e8
Idfa001 <idfa001@gmail.com>
2020-09-29 22:40:33 +0700

    29 sep 2020

---
+++ novice/07-02/ReadMe.md
@@ -0,0 +1,1 @@
+Catatan Keuangan V.1.3.2
--- novice/07-02/kelompok/kelompok/settings.py
+++ novice/07-02/kelompok/kelompok/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'de-DE'
+LANGUAGE_CODE = 'id-in'
 
 DATE_INPUT_FORMATS = [
     '%m/%d/%Y'
