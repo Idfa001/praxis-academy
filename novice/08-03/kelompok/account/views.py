465c50d3df55806a5fbafb8673ef1f7687ebed3d
Idfa001 <idfa001@gmail.com>
2020-10-07 10:49:01 +0700

    7 Okt 2020 (tambah login)

---
+++ novice/08-03/kelompok/account/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+from .models import *
+from .forms import CreateUserForm
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('/')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				return redirect('/account/')
+			else:
+				print(form.errors)
+		context = {'form':form}
+		return render(request, 'account/register.html', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('/')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('/')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'account/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('/account/')
---
+++ novice/08-03/kelompok/olshop/forms.py
@@ -0,0 +1,67 @@
+from django.forms import ModelForm
+from bootstrap_datepicker_plus import DatePickerInput
+from . import models
+
+
+class penjualan1f(ModelForm):
+    class Meta:
+        model = models.penjualan1m
+        exclude = [ 'saldo_awal' ]
+
+class saldoawalf(ModelForm):
+    class Meta:
+        model = models.saldoawalm
+        exclude = [ ]
+
+class penjualan2f(ModelForm):
+    class Meta:
+        model = models.penjualan2m
+        exclude = [ 'terima' ]
+        widgets = {
+            'jatuh_tempo': DatePickerInput(format='%d/%m/%Y'),
+        }
+class penjualan3f(ModelForm):
+    class Meta:
+        model = models.penjualan3m
+        exclude = [ 'terima' ]
+
+class utangf(ModelForm):
+    class Meta:
+        model = models.utangm
+        exclude = ['dibayar']
+
+class pend_lainf(ModelForm):
+    class Meta:
+        model = models.pend_lainm
+        exclude = [ 'terima' ]
+
+class pem_tunaif(ModelForm):
+    class Meta:
+        model = models.pem_tunaim
+        exclude = []
+
+class pem_kreditf(ModelForm):
+    class Meta:
+        model = models.pem_kreditm
+        exclude = [ 'dibayar1' ]
+
+class pem_lainf(ModelForm):
+    class Meta:
+        model = models.pem_lainm
+        exclude = [ 'dibayar2' ]
+
+class pembayaran_biayaf(ModelForm):
+    class Meta:
+        model = models.pembayaran_biayam
+        exclude = []
+
+class pembayaran_lainf(ModelForm):
+    class Meta:
+        model = models.pembayaran_lainm
+        exclude = [ 'dibayar3' ]
+
+class barangf(ModelForm):
+    class Meta:
+        model = models.barangm
+        exclude = []
+
